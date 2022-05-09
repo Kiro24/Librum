5867c51353cf422c51a2461ad7beb774c9b0794a
Kop3sh <kirollosadel24@yahoo.com>
2022-05-09 23:47:32 +0200

    fixed null check on images

--- lib/network/book_service.dart
+++ lib/network/book_service.dart
@@ -3,16 +3,18 @@ import 'book_model.dart';
 import 'model_response.dart';
 import 'model_converter.dart';
 import 'service_interface.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 part 'book_service.chopper.dart';
 
 const String apiUrl = 'https://www.googleapis.com/books/v1/';
 
+final _apiKey = dotenv.env['API_KEY'];
 // final apiKey = rootProject.file('.env');
 
 Request _addQuery(Request req) {
   final params = Map<String, dynamic>.from(req.parameters);
 
-  params['key'] = apiKey;
+  params['key'] = _apiKey;
 
   return req.copyWith(parameters: params);
 }
